ff2724218ee69a2a240ead14dd39ed288777fe80
takamuragit <97487688+takamuragit@users.noreply.github.com>
2022-06-28 16:43:19 +0900

    cleate_easy_nodejs_server

--- js/mapcontainer.js
+++
@@ -1,7 +0,0 @@
-function init() {
-    var map = L.map('mapcontainer');
-    map.setView([35.40, 136], 5);
-    L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
-        attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
-    }).addTo(map);
-}
