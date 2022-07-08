d041dd32b2fca1ad6966f769066ad94d545c99eb
jintajinta <jinta219@gmail.com>
2022-07-08 16:07:36 +0900

    test

--- todoapp/public/js/mapcontainer.js
+++ todoapp/public/js/mapcontainer.js
@@ -1,7 +1,7 @@
 function init() {
     var map = L.map('mapcontainer');
-    map.setView([35.40, 136], 5);
-    L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+    map.setView([35.4, 136], 5);
+    L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
     }).addTo(map);
 }
