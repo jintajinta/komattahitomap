8fba00cedbd1dc52110a8d931c71e9322df025c5
jintajinta <jinta219@gmail.com>
2022-07-11 17:16:58 +0900

    0711

--- todoapp/public/js/mapcontainer.js
+++ todoapp/public/js/mapcontainer.js
@@ -1,7 +1,35 @@
-function init() {
+let hello ="hello"
+let latitude
+let longitude
+function init(position) {
+    console.log(position)
     var map = L.map('mapcontainer');
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
     map.setView([35.4, 136], 5);
     L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
     }).addTo(map);
+    console.log(map)
+    console.log(hello)
+    hello="no"
+    console.log(hello)
+    function successCallback(position) {
+        // 緯度を取得し画面に表示
+        latitude = position.coords.latitude;
+        document.getElementById("latitude").innerHTML = latitude;
+        // 経度を取得し画面に表示
+        longitude = position.coords.longitude;
+        document.getElementById("longitude").innerHTML = longitude;
+        L.marker([latitude, longitude]).addTo(map).bindPopup("現在地").openPopup();
+    };
+    // 取得に失敗した場合の処理
+    function errorCallback(error) {
+        alert("位置情報が取得できませんでした");
+    };
 }
+
+function send(a){
+    console.log(a)
+}
+
+
--- todoapp/routes/map.js
+++ todoapp/routes/map.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
-    const userId = req.session.userid;
-    res.render("map",
+    knex("position_test")
+    .select("*")
+    .then(function (results) {
+        console.log(results[0].lat)
+        res.render("map",
         {
             title: 'Map',
-            id: userId,
+            position: results,
         })
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render("map",
+        {
+            title: 'Map',
+        })
+      });
 });
 
 
