099d63a8304349b94685a4f364c957bbe8f3b270
jintajinta <jinta219@gmail.com>
2022-07-14 14:56:49 +0900

    test position

--- todoapp/public/js/mapcontainer.js
+++ todoapp/public/js/mapcontainer.js
@@ -1,7 +1,10 @@
-let hello ="hello"
+let hello = "hello"
 let latitude
 let longitude
-function init(position) {
+function init() {
+    let position_data = document.getElementById("position").value;
+    console.log(position_data);
+    let position=JSON.parse(position_data);
     console.log(position)
     var map = L.map('mapcontainer');
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
@@ -9,10 +12,11 @@ function init(position) {
     L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
     }).addTo(map);
-    console.log(map)
-    console.log(hello)
-    hello="no"
-    console.log(hello)
+
+    for(i=0;i<=position.length-1;i++){
+        L.marker([position[i].lng, position[i].lat]).addTo(map).bindPopup("日本の端").openPopup();
+    }
+
     function successCallback(position) {
         // 緯度を取得し画面に表示
         latitude = position.coords.latitude;
@@ -24,11 +28,12 @@ function init(position) {
     };
     // 取得に失敗した場合の処理
     function errorCallback(error) {
+        console.log(error)
         alert("位置情報が取得できませんでした");
     };
 }
 
-function send(a){
+function send(a) {
     console.log(a)
 }
 
