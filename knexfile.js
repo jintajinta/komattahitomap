d6dc0be69de69804fd24fca2d18c5d4ee7c69a4f
jintajinta <jinta219@gmail.com>
2022-07-22 09:34:18 +0900

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -4,45 +4,34 @@ module.exports = {
 
   development: {
     client: "pg",
+    client: 'pg',
     connection: {
-      url: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-      database: "der4stj3obe9fd",
-      user: "cjlhzqazlofral",
-      password: "    e3323848f28cc0ca658ef68c53a95fd27be6d88c86e0f0cb6ffbae5c3e90c8a9",
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
   },
 
   staging: {
     client: "pg",
+    client: 'pg',
     connection: {
-      url: process.env.DATABASE_URL,
-      database: "der4stj3obe9fd",
-      user: "cjlhzqazlofral",
-      password: "e3323848f28cc0ca658ef68c53a95fd27be6d88c86e0f0cb6ffbae5c3e90c8a9",
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
   },
 
   production: {
-    client: "pg",
+    client: 'pg',
     connection: {
-      url: process.env.DATABASE_URL,
-      database: "der4stj3obe9fd",
-      user: "cjlhzqazlofral",
-      password: "e3323848f28cc0ca658ef68c53a95fd27be6d88c86e0f0cb6ffbae5c3e90c8a9",
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
   }
 
 };
