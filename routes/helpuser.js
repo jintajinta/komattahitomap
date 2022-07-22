d41d931c7ff1e675e159b1e1905b00b700f99f72
jintajinta <jinta219@gmail.com>
2022-07-22 12:15:49 +0900

    create help user page

--- routes/helpuser.js
+++ routes/helpuser.js
@@ -3,9 +3,25 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
+    knex("tasks")
+    .select("*")
+    .then(function (results) {
+        console.log(results[0].lat)
         res.render("helpuser",
         {
             title: '助ける人用ページ',
+            tasks: results,
         })
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render("助ける人用ページ",
+        {
+            title: 'Map',
+        })
+      });
 });
+
+
+
 module.exports = router;
