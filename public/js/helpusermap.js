10d7693b634ac92a1d8924b37cdeef004394761a
jintajinta <jinta219@gmail.com>
2022-07-22 12:22:48 +0900

    fix

--- public/js/helpusermap.js
+++ public/js/helpusermap.js
@@ -1,7 +1,7 @@
 let latitude
 let longitude
 function init() {
-    let task_data = document.getElementById("position").value;
+    let task_data = document.getElementById("tasks").value;
     let task=JSON.parse(task_data);
     var map = L.map('map');
     map.setView([36.00, 137], 10);
