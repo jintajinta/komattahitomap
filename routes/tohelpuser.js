042cfab129d92b19759ac11b7117b072a184af39
jintajinta <jinta219@gmail.com>
2022-07-22 11:50:24 +0900

    create post tohelpuser

--- routes/tohelpuser.js
+++ routes/tohelpuser.js
@@ -8,4 +8,21 @@ router.get('/', function (req, res, next) {
             title: '助けられる人用ページ',
         })
 });
+
+router.post('/', function (req, res, next) {
+    let mylng=req.body.mylng;
+    let mylat=req.body.mylat;
+    let contents=req.body.contents;
+    let classification=req.body.classification;
+    console.log(mylng+mylat+contents)
+    knex("position_test")
+        .insert({ lat: mylat, lng: mylng,contents:contents,class:classification})
+        .then(function () {
+            res.redirect('/tohelpuser');
+        })
+        .catch(function (err) {
+            console.error(err);
+            res.redirect('/tohelpuser');
+        });
+});
 module.exports = router;
