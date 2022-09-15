a582b655e24033c4dd6a1c4a48c0de5ff2d2d425
jintajinta <jinta219@gmail.com>
2022-09-15 11:31:45 +0900

    add deleteform

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use('/helpuser', require('./routes/helpuser'));
 app.use('/needhelpuser', require('./routes/needhelpuser'));
 app.use('/mypage', require('./routes/mypage'));
 app.use('/solvedform', require('./routes/solvedform'));
+app.use('/deleteform', require('./routes/deleteform'));
 app.use('/sh', require('./routes/sh'));
 app.use('/test', require('./routes/test'));
 
---
+++ routes/deleteform.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const knex = require('../db/knex');
+
+router.post('/', function (req, res, next) {
+    let taskid = req.body.taskid;
+    console.log(taskid)
+    knex("tasks").where('id',parseInt(taskid)).
+    update({
+        closed:'t'
+        })
+        .then(function () {
+            res.redirect('/test');
+        })
+        .catch(function (err) {
+            console.error(err);
+            res.redirect('/test');
+        });
+});
+
+module.exports = router;
