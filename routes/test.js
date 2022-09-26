39a8f96ffba1c1f3e4ec144d2bf6327b6ac2104f
jintajinta <jinta219@gmail.com>
2022-09-26 12:31:13 +0900

    test

--- routes/test.js
+++ routes/test.js
@@ -5,44 +5,18 @@ const knex = require('../db/knex');
 router.get('/', function (req, res, next) {
     const userId = req.session.userid;
     const isAuth = Boolean(userId);
-    knex("tasks").where({
-        user_id: userId,
-    })
-        .select("*")
-        .then(function (results) {
-            knex.select('contents')
-            .table('helps')
-            .innerJoin(
-                'tasks',
-                'helps.task_id',
-                '=',
-                'tasks.id'
-            ).then(function (resultusers) {
-                res.render('test', {
-                    title: '助けられる人用ページ',
-                    users: resultusers,
-                    tasks: results,
-                    isAuth: isAuth,
-                });
-            })
-            .catch(function (err) {
-                console.error(err);
-                res.render('test', {
-                    title: '助けられる人用ページ',
-                    tasks: results,
-                    isAuth: isAuth,
-                });
-            });
-        })
-        .catch(function (err) {
-            console.error(err);
-            res.render('test', {
-                title: '助けられる人用ページ',
-                isAuth: isAuth,
-            });
+    knex('helps')
+    .select('*').then(function (results) {
+        res.render('test', {
+            results: results,
         });
-
-    
+    })
+    .catch(function (err) {
+        console.error(err);
+        res.redirect('/map');
+    });
 
 });
+
+
 module.exports = router;
