e51dcf17bccf6e40e1c66693606a0f115bce7819
jintajinta <jinta219@gmail.com>
2022-09-30 14:35:06 +0900

    add

--- routes/helpuser.js
+++ routes/helpuser.js
@@ -6,34 +6,60 @@ const knex = require('../db/knex');
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
-    knex("tasks")
+  knex("tasks")
     .select("*")
-    .then(function (results) {
-        if(isAuth){
-        res.render("helpuser",
-        {
-            title: '助ける人用ページ',
-            tasks: results,
-            isAuth: isAuth,
-        })
-      }else{
+    .then(function (taskresults) {
+      if (isAuth) {
+        knex
+          .from('helps')
+          .innerJoin(
+            'tasks',
+            'helps.id',
+            'helps.task_id'
+          )
+          .select('*')
+          .where({
+            user_id: userId,
+          })
+          .then(function (helpresults) {
+            res.render("helpuser",
+            {
+              title: '助ける人用ページ',
+              tasks: taskresults,
+              helps: helpresults,
+              isAuth: isAuth,
+            })
+          })
+          .catch(function (err) {
+            res.render("helpuser",
+            {
+              title: '助ける人用ページ',
+              isAuth: isAuth,
+            })
+          });
+      } else {
         res.redirect('/');
       }
-      })
-      .catch(function (err) {
-        console.error(err);
-        if(isAuth){
+    })
+    .catch(function (err) {
+      console.error(err);
+      if (isAuth) {
         res.render("helpuser",
-        {
+          {
             title: '助ける人用ページ',
             isAuth: isAuth,
-        })
-      }else{
+          })
+      } else {
         res.redirect('/');
       }
-      });
+    });
 });
 
 
 
 module.exports = router;
+
+
+
+
+
--- routes/test.js
+++ routes/test.js
@@ -3,28 +3,28 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
-    const userId = req.session.userid;
-    const isAuth = Boolean(userId);
-    knex
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
+  knex
     .from('helps')
     .innerJoin(
-        'tasks', 
-        'helps.id', 
-        'helps.task_id'
-      )
-      .select('*')
-      .where({
-        user_id: userId,
-      })
-      .then(function (result) {
-        res.render("test", {
-            results:result
-          });
-})
-.catch(function (err) {
-    res.render("test", {
+      'tasks',
+      'helps.id',
+      'helps.task_id'
+    )
+    .select('*')
+    .where({
+      user_id: userId,
+    })
+    .then(function (result) {
+      res.render("test", {
+        results: result
       });
-});
+    })
+    .catch(function (err) {
+      res.render("test", {
+      });
+    });
 
 });
 
