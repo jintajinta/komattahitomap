5508dbd200151bba4edb36e929e193fc6eaad8b8
jintajinta <jinta219@gmail.com>
2022-10-24 16:49:07 +0900

    Update helpusermap.js

--- public/js/helpusermap.js
+++ public/js/helpusermap.js
@@ -16,7 +16,7 @@ function init() {
     for (i = 0; i <= task.length - 1; i++) {
         if (!Boolean(task[i].canceled) && !Boolean(task[i].completed)) {
             pingcontents = "分類:" + task[i].class + "<br>内容:" + task[i].content + "<br>位置の詳細:" + task[i].location_details + "<br>投稿者の見た目:" + task[i].appearance + "<br><input type = \"button\" value = \"助けに行く\" onClick = \"help_button_pressed(" + task[i].id + ")\">"
-            L.marker([task[i].lat, task[i].lng],{icon: L.spriteIcon('red')}).addTo(map).bindPopup(pingcontents).openPopup();
+            L.marker([task[i].lat, task[i].lng]).addTo(map).bindPopup(pingcontents).openPopup();
         }
     }
 
@@ -51,40 +51,4 @@ function help_button_pressed(id) {
 
 function tweet(){
     window.open('https://twitter.com/share?text=' + document.getElementById("address").value + '&url=' + 'https://enthousiaste-fromage-22608.herokuapp.com'  + '&hashtags=' + 'helpジャスフォ');
-}
-
-var L = global.L || require('leaflet');
-
-var fs = require('fs');
-
-var sources = {
-	blank: 'data:image/png;base64,' + fs.readFileSync(__dirname + '/blank.png', 'base64'),
-	sprite: 'data:image/png;base64,' + fs.readFileSync(__dirname + '/sprite.png', 'base64')
-};
-function css(string) {
-	var tag = document.createElement('style');
-	tag.innerHTML = string;
-	document.getElementsByTagName("head")[0].appendChild(tag);
-}
-
-L.spriteIcon = function(color) {
-  color = color || 'blue';
-  return L.icon({
-    className: "leaflet-sprite leaflet-sprite-" + color,
-    iconSize: [24, 41],
-    shadowsize: [41, 41],
-    iconAnchor: [12, 41],
-    iconUrl: sources.blank,
-    shadowUrl: L.Icon.Default.imagePath + "/marker-shadow.png"
-  });
-};
-
-var cssRegular = ".leaflet-sprite{background:url(" + sources.sprite + ") no-repeat top left;}\n.leaflet-sprite-blue{ background-position: 0 -132px; width: 41px; height: 41px; } \n.leaflet-sprite-green{ background-position: 0 -355px; width: 41px; height: 41px; } \n.leaflet-sprite-orange{ background-position: 0 -578px; width: 41px; height: 41px; } \n.leaflet-sprite-purple{ background-position: 0 -801px; width: 41px; height: 41px; } \n.leaflet-sprite-red{ background-position: 0 -1024px; width: 41px; height: 41px; } \n.leaflet-sprite-violet{ background-position: 0 -1247px; width: 41px; height: 41px; } \n.leaflet-sprite-yellow{ background-position: 0 -1470px; width: 41px; height: 41px; } ";
-
-var cssRetina = ".leaflet-sprite{\nbackground:url(" + sources.sprite + ") no-repeat top left;\nbackground-size:41px, 41px\n}\n .leaflet-sprite-blue{ background-position: 0 0; width: 41px; height: 41px; } \n .leaflet-sprite-green{ background-position: 0 -111px; width: 41px; height: 41px; } \n .leaflet-sprite-orange{ background-position: 0 -223px; width: 41px; height: 41px; } \n .leaflet-sprite-purple{ background-position: 0 -334px; width: 41px; height: 41px; } \n .leaflet-sprite-red{ background-position: 0 -446px; width: 41px; height: 41px; } \n .leaflet-sprite-violet{ background-position: 0 -557px; width: 41px; height: 41px; } \n .leaflet-sprite-yellow{ background-position: 0 -669px; width: 41px; height: 41px; } ";
-
-if (L.Browser.retina) {
-  css(cssRetina);
-} else {
-  css(cssRegular);
 }
