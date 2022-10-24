4ff7492cd0ea2f16571cd3837970bfc658990147
jintajinta <jinta219@gmail.com>
2022-10-24 12:35:38 +0900

    Update mapmein.js

--- public/js/mapmein.js
+++ public/js/mapmein.js
@@ -6,7 +6,12 @@ function init() {
     var map = L.map('map');
     map.setView([36.00, 137], 10);
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-    
+    navigator.geolocation.getCurrentPosition(
+        geoLoc => {
+          getAddress(geoLoc.coords);
+        },
+        err => console.error({err}),
+      );
     L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
     }).addTo(map);
@@ -28,21 +33,30 @@ function init() {
         document.getElementById("mylng").value = longitude;
         map.setView([latitude, longitude], 10);
         L.marker([latitude, longitude]).addTo(map).bindPopup("現在地").openPopup();
-        const url = new URL('https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress');
-        url.searchParams.set('lat', position.coords.latitude);
-        url.searchParams.set('lon', position.coords.longitude);
-        const res = await fetch(url.toString());
-        const json = await res.json();
-        const data = json.results;
-        const muniData = GSI.MUNI_ARRAY[json.results.muniCd];
-        const [prefCode, pref, muniCode, city] = muniData.split(',');
-        console.log(json)
     };
     // 取得に失敗した場合の処理
     function errorCallback(error) {
         console.log(error)
         alert("位置情報が取得できませんでした");
     };
+
+    const getAddress = async (coords) => {
+        // 逆ジオコーディング API
+        const url = new URL('https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress');
+        url.searchParams.set('lat', coords.latitude);
+        url.searchParams.set('lon', coords.longitude);
+        const res = await fetch(url.toString());
+        const json = await res.json();
+        const data = json.results;
+    
+        // 変換表から都道府県などを取得
+        const muniData = GSI.MUNI_ARRAY[json.results.muniCd];
+        // 都道府県コード,都道府県名,市区町村コード,市区町村名 に分割
+        const [prefCode, pref, muniCode, city] = muniData.split(',');
+    
+        // 画面に反映
+        console.log(json)
+      };
 
     
 }
