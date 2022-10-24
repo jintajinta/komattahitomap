3be786dabd1c8ec03d2e030d71974f922c8eba94
jintajinta <jinta219@gmail.com>
2022-10-24 15:00:24 +0900

    Update helpusermap.js

--- public/js/helpusermap.js
+++ public/js/helpusermap.js
@@ -29,7 +29,7 @@ function init() {
         let url = new URL('https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress');
     url.searchParams.set('lat', latitude);
     url.searchParams.set('lon', longitude);
-    fetch(url).then((response) => response.json()).then((data) => address.textContent=data);
+    fetch(url).then((response) => response.json()).then((data) => addressjson.textContent=data);
     };
     // 取得に失敗した場合の処理
     function errorCallback(error) {
