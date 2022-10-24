929a54ad9c327e7019c6b0100e64b9b684840a99
jintajinta <jinta219@gmail.com>
2022-10-24 15:16:27 +0900

    Update helpusermap.js

--- public/js/helpusermap.js
+++ public/js/helpusermap.js
@@ -14,22 +14,23 @@ function init() {
     }).addTo(map);
     let pingcontents = ""
     for (i = 0; i <= task.length - 1; i++) {
-        if(!Boolean(task[i].canceled)&&!Boolean(task[i].completed)){
-        pingcontents = "分類:" + task[i].class + "<br>内容:" + task[i].content + "<br>位置の詳細:" + task[i].location_details + "<br>投稿者の見た目:" + task[i].appearance + "<br><input type = \"button\" value = \"助けに行く\" onClick = \"help_button_pressed(" + task[i].id + ")\">"
-        L.marker([task[i].lat, task[i].lng]).addTo(map).bindPopup(pingcontents).openPopup();
+        if (!Boolean(task[i].canceled) && !Boolean(task[i].completed)) {
+            pingcontents = "分類:" + task[i].class + "<br>内容:" + task[i].content + "<br>位置の詳細:" + task[i].location_details + "<br>投稿者の見た目:" + task[i].appearance + "<br><input type = \"button\" value = \"助けに行く\" onClick = \"help_button_pressed(" + task[i].id + ")\">"
+            L.marker([task[i].lat, task[i].lng]).addTo(map).bindPopup(pingcontents).openPopup();
         }
     }
 
-    function successCallback(position) {
+    async function successCallback(position) {
         // 緯度を取得し画面に表示
         latitude = position.coords.latitude;
         // 経度を取得し画面に表示
         longitude = position.coords.longitude;
         map.setView([latitude, longitude], 10);
         let url = new URL('https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress');
-    url.searchParams.set('lat', latitude);
-    url.searchParams.set('lon', longitude);
-    fetch(url).then((response) => response.json()).then((data) => console.log(data));
+        url.searchParams.set('lat', latitude);
+        url.searchParams.set('lon', longitude);
+        const res=await fetch(url);
+        console.log(res)
     };
     // 取得に失敗した場合の処理
     function errorCallback(error) {
