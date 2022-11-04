ea839666af099c97e2a1366591ca20518a2c5f9e
jintajinta <jinta219@gmail.com>
2022-11-04 11:01:26 +0900

    fix

--- routes/cancelform.js
+++ routes/cancelform.js
@@ -4,10 +4,15 @@ const knex = require('../db/knex');
 
 router.post('/', function (req, res, next) {
     let helpid = req.body.helpid;
+    let taskid = req.body.taskid;
     knex("helps").where('helpid', parseInt(helpid)).
         del()
         .then(function () {
-            res.redirect('/');
+            knex
+            .raw('update tasks set number_of_applicants =number_of_applicants-1 where id=?;',[taskid])
+            .then(function(){
+                res.redirect('/');
+            })
         })
         .catch(function (err) {
             console.error(err);
